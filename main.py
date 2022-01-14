446d3e4d9651894d06bacca336c0e6e703f0aaf5
Milan <58393068+Desoxyr@users.noreply.github.com>
2022-01-14 01:43:42 +0100

    Working Neural Net

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ import os
 from neatnetwork import NeatNetwork
 
 if __name__ == '__main__':
-    # game = Game()
-    # game.run_game()
-
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'config-feedforward.txt')
     neat = NeatNetwork(True)
